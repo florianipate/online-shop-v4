5566d2b53c19d6ede2c5bdcd8e3c123a9f4b7103
Florian Ipate <florian.ipate@gmail.com>
2022-08-22 19:46:05 +0100

    update Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import Header from './routes/header/header.component';
 
 import Home from './routes/home/home.component';
 import NewIn from './routes/newIn/newIn.component';
 const App = () => {
   return (
     <Routes>
-      <Route index element = { <Home /> } />
-      <Route path='/newIn' element = { <NewIn /> } />
+      <Route path='/' element={ <Header />}>
+        <Route index element = { <Home /> } />
+        <Route path='/newIn' element = { <NewIn /> } />
+      </Route>
     </Routes> 
   );
 }
---
+++ src/routes/header/header.component.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+const Header = () =>{
+    return(
+        <div>
+            <div>
+                <h1>I Am the Heade</h1>
+            </div>
+            <Outlet />
+        </div>
+    );
+}
+export default Header;
