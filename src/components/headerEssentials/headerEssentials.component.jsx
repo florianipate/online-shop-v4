89fa5a4b112b11189998c60e677cec590d06daee
Florian Ipate <florian.ipate@gmail.com>
2022-08-23 16:40:32 +0100

    added top Header essential Links

---
+++ src/components/headerEssentials/headerEssentials.component.jsx
@@ -0,0 +1,6 @@
+const HeaderEssentials = () =>{
+    return(
+        <div><p>HeaderEssentials</p></div>
+    );
+}
+export default HeaderEssentials;
--- src/routes/header/header.component.jsx
+++ src/routes/header/header.component.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom"
+import HeaderEssentials from "../../components/headerEssentials/headerEssentials.component";
+
 const Header = () =>{
     return(
         <div>
             <div>
+                <HeaderEssentials />
                 <h1>I Am the Heade</h1>
             </div>
             <Outlet />
