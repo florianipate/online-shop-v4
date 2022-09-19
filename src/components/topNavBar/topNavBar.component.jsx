f019c31947d122c7f9ff4645da2c3b5abf90593a
Florian Ipate <florian.ipate@gmail.com>
2022-09-19 08:05:37 +0100

    update nav links

--- src/components/topNavBar/topNavBar.component.jsx
+++ src/components/topNavBar/topNavBar.component.jsx
@@ -3,19 +3,19 @@ import { NavLinksContainer, NavLink } from './topNavBar.style';
 const TopNavContainer = () => {
     return (
         <NavLinksContainer>
-            <NavLink to = '/newin'>
+            <NavLink to = '/link1'>
                 LINK 1
             </NavLink>
-            <NavLink to = '/newin'>
+            <NavLink to = '/link2'>
                 LINK 2
             </NavLink>
-            <NavLink to = '/newin'>
+            <NavLink to = '/link3'>
                 LINK 3
             </NavLink>
-            <NavLink to = '/newin'>
+            <NavLink to = '/link4'>
                 LINK 4
             </NavLink>
-            <NavLink to = '/newin'>
+            <NavLink to = '/link5'>
                 LINK 5
             </NavLink>           
         </NavLinksContainer>
