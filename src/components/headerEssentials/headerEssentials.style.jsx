671f2b9d21e6afd20e6d721371e5e2d8f3bac3a9
Florian Ipate <florian.ipate@gmail.com>
2022-09-25 12:48:26 +0100

    remove space

--- src/components/headerEssentials/headerEssentials.style.jsx
+++ src/components/headerEssentials/headerEssentials.style.jsx
@@ -39,8 +39,6 @@ export const LinkItem = styled.li`
         display: inline-block;
         margin-right: 6px;
     }`
-
-    
 export const DropdownArrow = styled.b`
     // margin-top:-3px;
     display: flex;
